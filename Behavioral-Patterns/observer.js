5490bfc993a0939925bcbe826a4c3dfaf4c9c82b
zzl <www.01569@qq.com>
2020-10-16 17:06:26 +0800

    17.06

---
+++ Behavioral-Patterns/observer.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: zouzongliang
+ * @Date: 2020-10-15 10:11:38
+ * @LastEditTime: 2020-10-16 16:26:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @file: content
+ */
+
+
+/**
+ * 行为模式:
+ * 
+ * 定义一种一对多的模式
+ * 
+ * 
+ */
